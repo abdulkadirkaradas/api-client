aa38db96b2d369fe36b0973137e8c0e077719837
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-18 01:30:14 +0300

    Enhance AuthorizationServiceConfig interface with AxiosRequestConfig and APIClientConfig

--- src/interfaces/auth.ts
+++ src/interfaces/auth.ts
@@ -1,12 +1,20 @@
+import { AxiosRequestConfig } from "axios";
 import { StorageType } from "./storage";
+import { APIClientConfig } from "./core";
+
+export interface AuthorizationServiceConfig {
+  url: string;
+  data?: AxiosRequestConfig["data"];
+  config?: APIClientConfig;
+}
 
 export interface AuthorizationTokenConfig {
-    requestTokenConfig?: {
-        accessTokenName?: string | null;
-        refreshTokenName?: string | null;
-    };
-    tokenStorageType?: {
-        accessToken?: StorageType | null;
-        refreshToken?: StorageType | null;
-    }
+  requestTokenConfig?: {
+    accessTokenName?: string | null;
+    refreshTokenName?: string | null;
+  };
+  tokenStorageType?: {
+    accessToken?: StorageType | null;
+    refreshToken?: StorageType | null;
+  };
 }
