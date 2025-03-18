b3b1dcdc5da26a27f9ec4ad2bdfa4c92643bf48d
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-18 21:01:52 +0300

    Add request and response interceptors to APIClient for enhanced header management

--- src/core/apiClient.ts
+++ src/core/apiClient.ts
@@ -1,13 +1,20 @@
+import { RequestInterceptor } from "../interceptors/requestInterceptor";
+import { ResponseInterceptor } from "../interceptors/responseInterceptors";
 import { APIClientConfig } from "../interfaces/core";
 import { Methods } from "../methods/methods";
 import { APIClientConstructor } from "./apiClientConstructor";
 
 export class APIClient extends APIClientConstructor {
   public methods: Methods;
+  public interceptor: { request: RequestInterceptor, response: ResponseInterceptor };
 
   constructor(config: APIClientConfig) {
     super(config);
 
     this.methods = new Methods(this.client);
+    this.interceptor = {
+      request: new RequestInterceptor(this.client, config?.headers || {}),
+      response: new ResponseInterceptor(this.client, config?.headers || {}),
+    };
   }
 }
