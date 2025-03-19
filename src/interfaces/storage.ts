c0e14659a0e5ffd6edb8077f50993a1a0b06d0ed
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-20 00:16:06 +0300

    Add IStorage interface and update StorageType definition

--- src/interfaces/storage.ts
+++ src/interfaces/storage.ts
@@ -1,1 +1,8 @@
-export type StorageType = 'localStorage' | 'sessionStorage' | 'cookie' | 'json';
+export type StorageType = "localStorage" | "sessionStorage" | "cookie" | "json";
+
+export interface IStorage {
+  set(key: string, token: string): void;
+  get(key: string): string | null;
+  remove(key: string): void;
+  clear(): void;
+}
