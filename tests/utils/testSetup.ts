2cb46506d8518b48842c7a4f5b987354b032ac8b
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-20 01:16:40 +0300

    Refactor TestSetup to integrate ClientStorageFactory and ClientServices for improved API client creation

--- tests/utils/testSetup.ts
+++ tests/utils/testSetup.ts
@@ -1,12 +1,16 @@
-import MockAdapter from 'axios-mock-adapter';
-import { APIClient } from '../../src/core/apiClient';
-import { APIClientConfig } from '../../src/interfaces/core';
-import { createAPIClient } from '../../src/index';
+import MockAdapter from "axios-mock-adapter";
+import { APIClient } from "../../src/core/apiClient";
+import { APIClientConfig } from "../../src/interfaces/core";
+import { createAPIClient } from "../../src/index";
+import { ClientStorageFactory } from "../../src/utils/storage/client/storageFactory";
+import { ClientServices } from "../../src/core/services/client/clientService";
 
 export class TestSetup {
   public config: APIClientConfig;
   public mock: MockAdapter;
   public instance: APIClient;
+  public clientStorage: ClientStorageFactory;
+  public clientService: ClientServices;
 
   constructor() {
     this.config = {
@@ -19,14 +23,13 @@ export class TestSetup {
           accessTokenName: "access_token",
           refreshTokenName: "refresh_token",
         },
-        tokenStorageType: {
-          accessToken: "localStorage",
-          refreshToken: "localStorage",
-        },
       },
     };
 
-    this.instance = createAPIClient(this.config);
+    const create = createAPIClient(this.config);
+    this.instance = create.ApiClient;
+    this.clientService = create.Services.client;
+    this.clientStorage = create.Storage.client;
     this.mock = new MockAdapter(this.instance.getInstance());
   }
 
