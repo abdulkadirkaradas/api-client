c573347fc54076c18330b1f053a003926331d793
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-20 01:17:01 +0300

    Enhance createAPIClient to initialize ClientServices and ClientStorageFactory for improved API client functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import { APIClient } from "./core/apiClient";
+import { ClientServices } from "./core/services/client/clientService";
 import { APIClientConfig } from "./interfaces/core";
+import { ClientStorageFactory } from "./utils/storage/client/storageFactory";
 
 export const createAPIClient = (config: APIClientConfig) => {
-    return new APIClient(config);
+  const ApiClient = new APIClient(config);
+  const Services = {
+    client: new ClientServices({
+      client: ApiClient.getInstance(),
+      tokenConfig: config.tokenConfig || {},
+    }),
+  };
+  const Storage = {
+    client: new ClientStorageFactory(),
+  };
+
+  return {
+    ApiClient,
+    Services,
+    Storage,
+  };
 };
