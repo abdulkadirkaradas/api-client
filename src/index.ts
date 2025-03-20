a31fa1efec8ed0fa58dbe84fa5aa5fa3b75d017a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-20 15:03:39 +0300

    Integrate MethodGenerator into createAPIClient for dynamic method generation

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { APIClient } from "./core/apiClient";
 import { ClientServices } from "./core/services/client/clientService";
 import { APIClientConfig } from "./interfaces/core";
 import { ClientStorageFactory } from "./utils/storage/client/storageFactory";
+import { MethodGenerator as Generator } from './methods/generator';
 
 export const createAPIClient = (config: APIClientConfig) => {
   const ApiClient = new APIClient(config);
+  const MethodGenerator = new Generator(ApiClient.getInstance());
   const Services = {
     client: new ClientServices({
       client: ApiClient.getInstance(),
@@ -17,6 +19,7 @@ export const createAPIClient = (config: APIClientConfig) => {
 
   return {
     ApiClient,
+    MethodGenerator,
     Services,
     Storage,
   };
