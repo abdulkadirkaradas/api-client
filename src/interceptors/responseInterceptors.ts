b71942b8ef9d211bc2517f196c3ae848ef14a1a8
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-21 22:50:13 +0300

    Refactor ResponseInterceptor to remove header management and improve error handling

--- src/interceptors/responseInterceptors.ts
+++ src/interceptors/responseInterceptors.ts
@@ -1,52 +1,20 @@
 import { AxiosInstance, AxiosResponse, RawAxiosResponseHeaders } from "axios";
 import { InterceptorConstructor } from "./interceptorConstructor";
 import { CommonResponseHeadersList } from "../interfaces/test";
+import { handleAPIError } from "../utils/error/errorHandler";
 
 export class ResponseInterceptor extends InterceptorConstructor {
-  private headers: RawAxiosResponseHeaders = {};
-
-  constructor(client: AxiosInstance, headers: RawAxiosResponseHeaders = {}) {
+  constructor(client: AxiosInstance) {
     super(client);
 
-    this.headers = headers;
     this.registerInterceptor();
   }
 
-  /**
-   * Updates the response headers manually
-   *
-   * @param headers
-   */
-  public setHeaders(headers: RawAxiosResponseHeaders) {
-    this.headers = headers;
-  }
-
-  /**
-   * Returns the value of the header
-   *
-   * @param header
-   * @returns
-   */
-  public getHeaderValue(header: CommonResponseHeadersList) {
-    return this.headers[header];
-  }
-
-  /**
-   * Registers the interceptor and adds the provided headers to the response headers
-   */
   private registerInterceptor() {
     this.client.interceptors.response.use(
-      (response: AxiosResponse) => {
-        for (const header in this.headers) {
-          if (this.headers.hasOwnProperty(header)) {
-            response.headers[header] = this.headers[header];
-          }
-        }
-
-        return response;
-      },
+      (response) => response,
       (error) => {
-        Promise.reject(error);
+        handleAPIError(error);
       }
     );
   }
