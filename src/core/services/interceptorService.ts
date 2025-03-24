b4f0b7c7d8cb460b6dd37dee533aee73f400aa9d
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-25 01:58:28 +0300

    Add InterceptorService to manage request and response interceptors

---
+++ src/core/services/interceptorService.ts
@@ -0,0 +1,18 @@
+import { IInterceptorConfig } from "../../interfaces/interceptors";
+import { RequestInterceptor } from "../../interceptors/requestInterceptor";
+import { ResponseInterceptor } from "../../interceptors/responseInterceptors";
+
+export class InterceptorService {
+  public request: RequestInterceptor;
+  public response: ResponseInterceptor;
+
+  constructor(config: IInterceptorConfig) {
+    this.request = new RequestInterceptor({
+      client: config.client,
+      eventBus: config.eventBus,
+      headers: config.headers,
+    });
+
+    this.response = new ResponseInterceptor(config.client);
+  }
+}
