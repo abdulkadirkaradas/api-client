7046ba7472197f546f2e0cfcabd2cb9da7ddba16
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-25 01:55:52 +0300

    Add EventBus integration to APIClientConstructor for event handling

--- src/core/apiClientConstructor.ts
+++ src/core/apiClientConstructor.ts
@@ -1,14 +1,21 @@
 import axios, { AxiosInstance } from 'axios';
 import { APIClientConfig } from '../interfaces/core';
+import { EventBus } from '../utils/eventBus/EventBus';
 
 export class APIClientConstructor {
     protected client: AxiosInstance;
+    protected eventBus: EventBus;
 
     constructor(config: APIClientConfig) {
         this.client = axios.create(config);
+        this.eventBus = new EventBus();
     }
 
     public getInstance() {
         return this.client as AxiosInstance;
+    }
+
+    public getEventBusInstance() {
+        return this.eventBus as EventBus;
     }
 }
