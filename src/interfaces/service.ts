6c559f585812db6d9e0cee9f60cef3728209d25d
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-25 01:59:11 +0300

    Add eventBus property to IServiceConstructor interface

--- src/interfaces/service.ts
+++ src/interfaces/service.ts
@@ -1,8 +1,10 @@
 import { AxiosInstance } from "axios";
 import { AuthorizationTokenConfig } from "./auth";
 import { StorageType } from "./storage";
+import { EventBus } from "../utils/eventBus/EventBus";
 
 export interface IServiceConstructor {
     client: AxiosInstance;
+    eventBus: EventBus;
     tokenConfig?: AuthorizationTokenConfig;
 }
