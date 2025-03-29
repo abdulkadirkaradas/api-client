bfba50b39c85a3091dfc71621df39fd63565d4c5
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-29 23:55:38 +0300

    Add EventBus initialization to API client creation

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { ClientServices } from "./core/services/client/clientService";
 import { APIClientConfig } from "./interfaces/core";
 import { ClientStorageFactory } from "./utils/storage/client/storageFactory";
 import { MethodGenerator as Generator } from './methods/generator';
+import { EventBus as EventBusClass } from './utils/eventBus/EventBus';
 
 export const createAPIClient = (config: APIClientConfig) => {
   const ApiClient = new APIClient(config);
+  const EventBus = new EventBusClass();
   const MethodGenerator = new Generator(ApiClient.getInstance());
   const Services = {
     client: new ClientServices({
@@ -21,6 +23,7 @@ export const createAPIClient = (config: APIClientConfig) => {
 
   return {
     ApiClient,
+    EventBus,
     MethodGenerator,
     Services,
     Storage,
