86af89f62403f396bb1cd7a6fee88c6f47c0209b
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-03-29 23:55:45 +0300

    Add EventBus initialization to TestSetup class for enhanced testing capabilities

--- tests/utils/testSetup.ts
+++ tests/utils/testSetup.ts
@@ -1,15 +1,17 @@
-import MockAdapter from "axios-mock-adapter";
-import { APIClient } from "../../src/core/apiClient";
-import { APIClientConfig } from "../../src/interfaces/core";
-import { createAPIClient } from "../../src/index";
-import { ClientStorageFactory } from "../../src/utils/storage/client/storageFactory";
-import { ClientServices } from "../../src/core/services/client/clientService";
-import { MethodGenerator } from "../../src/methods/generator";
+import MockAdapter from 'axios-mock-adapter';
+import { APIClient } from '../../src/core/apiClient';
+import { APIClientConfig } from '../../src/interfaces/core';
+import { ClientServices } from '../../src/core/services/client/clientService';
+import { ClientStorageFactory } from '../../src/utils/storage/client/storageFactory';
+import { createAPIClient } from '../../src/index';
+import { EventBus } from '../../src/utils/eventBus/EventBus';
+import { MethodGenerator } from '../../src/methods/generator';
 
 export class TestSetup {
   public config: APIClientConfig;
   public mock: MockAdapter;
   public instance: APIClient;
+  public eventBus: EventBus;
   public clientStorage: ClientStorageFactory;
   public clientService: ClientServices;
   public methodGenerator: MethodGenerator;
@@ -28,6 +30,7 @@ export class TestSetup {
 
     const create = createAPIClient(this.config);
     this.instance = create.ApiClient;
+    this.eventBus = create.EventBus;
     this.methodGenerator = create.MethodGenerator;
     this.clientService = create.Services.client;
     this.clientStorage = create.Storage.client;
