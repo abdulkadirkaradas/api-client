864e29eb07a73333c657d1365a278aa04ead8161
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-06-11 13:10:22 +0300

    Refactor APIClientConstructor to use excluded configuration for Axios instance initialization

--- src/core/apiClientConstructor.ts
+++ src/core/apiClientConstructor.ts
@@ -1,6 +1,11 @@
-import axios, { AxiosInstance } from 'axios';
-import { APIClientConfig } from '../interfaces/core';
-import { EventBus } from '../utils/eventBus/EventBus';
+import axios, { AxiosInstance } from "axios";
+import { APIClientConfig } from "../interfaces/core";
+import { EventBus } from "../utils/eventBus/EventBus";
+
+type ExcludedAuthClientConfig = Omit<
+  APIClientConfig,
+  "authProtocol" | "tokenConfig"
+>;
 
 /**
  * The `APIClientConstructor` class is a base class for creating an API client.
@@ -18,8 +23,10 @@ export class APIClientConstructor {
    * @param config - Configuration object for initializing the API client.
    */
   constructor(config: APIClientConfig) {
+    const excludedConfig: ExcludedAuthClientConfig = config;
+
     this.config = config;
-    this.client = axios.create(config);
+    this.client = axios.create(excludedConfig);
     this.eventBus = new EventBus();
   }
 
